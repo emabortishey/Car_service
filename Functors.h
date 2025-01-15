d31bb9be0f7a6d0a237778c0056c892b8ee99c6f
fartb <fartbreather228@gmail.com>
2025-01-15 23:57:33 +0300

    добавлено 4 функтора для сортировки по возрастанию для будущих методов сортировки

--- Functors.h
+++ Functors.h
@@ -24,3 +24,28 @@ class search_volume
 public:
 	bool operator()(int volume1, int volume2) { return volume1 == volume2; }
 };
+
+
+class sort_name
+{
+public:
+	bool operator()(string name1, string name2) { return name1 > name2; }
+};
+
+class sort_price
+{
+public:
+	bool operator()(int price1, int price2) { return price1 > price2; }
+};
+
+class sort_year
+{
+public:
+	bool operator()(int year1, int year2) { return year1 > year2; }
+};
+
+class sort_volume
+{
+public:
+	bool operator()(int volume1, int volume2) { return volume1 > volume2; }
+};
