6170f1dafaf2354440e2f64b6797352f7026e2cf
fartb <fartbreather228@gmail.com>
2025-01-15 22:46:09 +0300

    добавлены методы показа авто

--- Car_dealership.h
+++ Car_dealership.h
@@ -16,14 +16,27 @@ class Car_dealership
 		int volume;
 
 		Car(string brand_P, int price_P, int year_P, int volume_P) : brand{ brand_P }, price{ price_P }, year{ year_P }, volume{ volume_P } { };
+
+		void show_car() { cout << "\nМарка авто: " << brand << "\nЦена: " << price << "\nГод выпуска: " << year << "\nОбъем двигателя: " << volume; }
 	};
 
 	friend Car;
 
-	list<Car> Cars; 
+	list<Car> cars; 
 
 public:
 	Car_dealership() = default;
-	Car_dealership(initializer_list<Car> init_list) : Cars{ init_list } { };
+	Car_dealership(initializer_list<Car> init_list) : cars{ init_list } { };
+
+	void show_all_cars()
+	{
+		for (auto buff : cars)
+		{
+			buff.show_car();
+			cout << '\n';
+		}
+	}
+
+
 
 };
