bae3667ee3c929140e18a41ec33116f923f4dcb8
fartb <fartbreather228@gmail.com>
2025-01-15 23:52:52 +0300

    в отдельный хедер добавлено 4 функтора для будущих методов поиска

--- Functors.h
+++ Functors.h
@@ -4,5 +4,23 @@
 class search_name
 {
 public:
-	
+	bool operator()(string name1, string name2) { return name1 == name2; }
+};
+
+class search_price
+{
+public:
+	bool operator()(int price1, int price2) { return price1 == price2; }
+};
+
+class search_year
+{
+public:
+	bool operator()(int year1, int year2) { return year1 == year2; }
+};
+
+class search_volume
+{
+public:
+	bool operator()(int volume1, int volume2) { return volume1 == volume2; }
 };
