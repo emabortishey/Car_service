f6552e3e16f4b1418fd28105e5ee0b21ae4f8d6d
fartb <fartbreather228@gmail.com>
2025-01-16 12:06:03 +0300

    методы класса car dealership вынесены в срр файл

--- Car_dealership.cpp
+++ Car_dealership.cpp
@@ -1,1 +1,101 @@
 #include "Car_dealership.h"
+
+void Car_dealership::show_all_cars()
+{
+	for (auto buff : cars)
+	{
+		buff.show_car();
+		cout << '\n';
+	}
+}
+
+void Car_dealership::add(initializer_list<Car> init_list)
+{
+	// цикличный пушбек элементов списка инициализаторов
+	for (auto buff : init_list)
+	{
+		cars.push_back(buff);
+	}
+}
+
+void Car_dealership::remove(initializer_list<Car> init_list)
+{
+	// цикл по списку инициализаторов
+	for (Car buff : init_list)
+	{
+		// т.к. в векторе нельзя удалить определенный
+		// элемент, я использую метод эрейс, который
+		// принимает итератор на элемент, для получения
+		// которого я использую алгоритм файнд иф, в который
+		// передаю унарный функтор поиска подходящей машины
+		cars.erase(find_if(cars.begin(), cars.end(), search_car(buff)));
+	}
+}
+
+void Car_dealership::sort_by_smth(int by_what)
+{
+	switch (by_what)
+	{
+	case BY_BRAND:
+	{
+		sort(cars.begin(), cars.end(), sort_name());
+
+		break;
+	}
+	case BY_PRICE:
+	{
+		sort(cars.begin(), cars.end(), sort_price());
+
+		break;
+	}
+	case BY_YEAR:
+	{
+		sort(cars.begin(), cars.end(), sort_year());
+
+		break;
+	}
+	case BY_VOLUME:
+	{
+		sort(cars.begin(), cars.end(), sort_volume());
+
+		break;
+	}
+	}
+}
+
+vector<Car> Car_dealership::search_by_name(string name_P)
+{
+	vector<Car> found_cars(cars.size());
+
+	// используется алгоритм копи иф, принимающий унарный функетр
+	copy_if(cars.begin(), cars.end(), found_cars.begin(), search_name(name_P));
+
+	return found_cars;
+}
+
+vector<Car> Car_dealership::search_by_price(int price_P)
+{
+	vector<Car> found_cars(cars.size());
+
+	copy_if(cars.begin(), cars.end(), found_cars.begin(), search_price(price_P));
+
+	return found_cars;
+}
+
+vector<Car> Car_dealership::search_by_year(int year_P)
+{
+	vector<Car> found_cars(cars.size());
+
+	copy_if(cars.begin(), cars.end(), found_cars.begin(), search_year(year_P));
+
+	return found_cars;
+}
+
+vector<Car> Car_dealership::search_by_volume(int volume_P)
+{
+	vector<Car> found_cars(cars.size());
+
+	copy_if(cars.begin(), cars.end(), found_cars.begin(), search_volume(volume_P));
+
+	return found_cars;
+}
--- Car_dealership.h
+++ Car_dealership.h
@@ -17,43 +17,17 @@ public:
 
 	// цикличный вызов метода показа 
 	// дл€ каждого обьекта вектора 
-	void show_all_cars()
-	{
-		for (auto buff : cars)
-		{
-			buff.show_car();
-			cout << '\n';
-		}
-	}
+	void show_all_cars();
 
 	// не помню зачем добавл€ла перегрузку
 	// с параметром списка инициализаторов
 	// но решила оставить на вс€кий случай
-	void add(initializer_list<Car> init_list)
-	{
-		// цикличный пушбек элементов списка инициализаторов
-		for (auto buff : init_list)
-		{
-			cars.push_back(buff);
-		}
-	}
+	void add(initializer_list<Car> init_list);
 
 	// пушбек элемента в вектор
 	void add(Car obj) { cars.push_back(obj); }
 
-	void remove(initializer_list<Car> init_list)
-	{
-		// цикл по списку инициализаторов
-		for (Car buff : init_list)
-		{
-			// т.к. в векторе нельз€ удалить определенный
-			// элемент, € использую метод эрейс, который
-			// принимает итератор на элемент, дл€ получени€
-			// которого € использую алгоритм файнд иф, в который
-			// передаю унарный функтор поиска подход€щей машины
-			cars.erase(find_if(cars.begin(), cars.end(), search_car(buff)));
-		}
-	}
+	void remove(initializer_list<Car> init_list);
 
 	// то же самое что и с прошлой перегрузкй, 
 	// но еденичный вызов метода вектора
@@ -61,36 +35,7 @@ public:
 
 	// метод сортировки, обьедин€ющий все4 критери€
 	// по которым будет выполн€тьс€ сортировка
-	void sort_by_smth(int by_what)
-	{
-		switch (by_what)
-		{
-		case BY_BRAND:
-		{
-			sort(cars.begin(), cars.end(), sort_name());
-
-			break;
-		}
-		case BY_PRICE:
-		{
-			sort(cars.begin(), cars.end(), sort_price());
-
-			break;
-		}
-		case BY_YEAR:
-		{
-			sort(cars.begin(), cars.end(), sort_year());
-
-			break;
-		}
-		case BY_VOLUME:
-		{
-			sort(cars.begin(), cars.end(), sort_volume());
-
-			break;
-		}
-		}
-	}
+	void sort_by_smth(int by_what);
 
 	// поиск по имени (не обьеденила эти методы
 	// т.к. у 1 атрибута отличный от остальных тип)
@@ -98,40 +43,11 @@ public:
 	// каждый метод создает буферный вектор со всеми
 	// найденными машинами и возвращает его, дл€
 	// дальнейшей рабта с ним пользовател€
-	vector<Car> search_by_name(string name_P)
-	{
-		vector<Car> found_cars(cars.size());
-		
-		// используетс€ алгоритм копи иф, принимающий унарный функетр
-		copy_if(cars.begin(), cars.end(), found_cars.begin(), search_name(name_P)); 	
-			
-		return found_cars;
-	}
+	vector<Car> search_by_name(string name_P);
 
-	vector<Car> search_by_price(int price_P)
-	{
-		vector<Car> found_cars(cars.size());
+	vector<Car> search_by_price(int price_P);
 
-		copy_if(cars.begin(), cars.end(), found_cars.begin(), search_price(price_P));
+	vector<Car> search_by_year(int year_P);
 
-		return found_cars;
-	}
-
-	vector<Car> search_by_year(int year_P)
-	{
-		vector<Car> found_cars(cars.size());
-
-		copy_if(cars.begin(), cars.end(), found_cars.begin(), search_year(year_P));
-
-		return found_cars;
-	}
-
-	vector<Car> search_by_volume(int volume_P)
-	{
-		vector<Car> found_cars(cars.size());
-
-		copy_if(cars.begin(), cars.end(), found_cars.begin(), search_volume(volume_P));
-
-		return found_cars;
-	}
+	vector<Car> search_by_volume(int volume_P);
 };
